d63083df601458990f589da47bccd8382581c59f
jng90 <jng@o2.pl>
2021-12-18 15:30:38 +0100

    Gifts App - deploy

--- src/main/java/pl/sda/xmasgifts/service/XmasGiftsService.java
+++ src/main/java/pl/sda/xmasgifts/service/XmasGiftsService.java
@@ -5,12 +5,13 @@ import pl.sda.xmasgifts.entity.Wish;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface XmasGiftsService {
 
     Person addPerson(Person person);
 
-    Optional<Wish> addPersonWish(Wish wish, long personId);
+    Optional<Wish> addPersonWish(Wish wish, UUID personId);
 
     Wish fulfillWishByPerson(long wishId, long personId);
 

5c9d0ef71226ddc007189968d456cb6d0385f9eb
jng90 <jng@o2.pl>
2021-12-18 14:32:32 +0100

    Gifts App - progress v3

--- src/main/java/pl/sda/xmasgifts/controller/XmasGiftController.java
+++ src/main/java/pl/sda/xmasgifts/controller/XmasGiftController.java
@@ -1,13 +1,50 @@
 package pl.sda.xmasgifts.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.xmasgifts.entity.Person;
+import pl.sda.xmasgifts.service.XmasGiftsService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class XmasGiftController {
 
+    private final XmasGiftsService xmasGiftsService;
+
+    public XmasGiftController(XmasGiftsService xmasGiftsService) {
+        this.xmasGiftsService = xmasGiftsService;
+    }
+
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/user/add")
+    public String addUserForm(){
+        return "add-user-form";
+    }
+
+    @PostMapping("/user/add")
+    public ModelAndView addUser(@ModelAttribute Person person, HttpServletResponse response){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("add-user-confirm");
+        final Person user = xmasGiftsService.addPerson(person);
+        modelAndView.getModelMap().addAttribute("person", user);
+        Cookie cookie = new Cookie("xmas-user-id", user.getId().toString());
+        response.addCookie(cookie);
+        return modelAndView;
+    }
+
+    @GetMapping("/user/list")
+    public String listUsers(Model model){
+        model.addAttribute("people", xmasGiftsService.findAllPersons());
+        return "users-list";
     }
 }
